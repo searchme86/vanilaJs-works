5010396d7ebe72afa25311d7cf711b2041b215af
searchme86 <searchme86@gmail.com>
2022-11-19 17:01:17 +0900

    new apps

---
+++ webpack.dev.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.txt/,
+        type: 'asset/source',
+      },
+      {
+        test: /\.(png|jpg)$/,
+        type: 'asset',
+      },
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+        // include: [path.resolve(__dirname, 'src')],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist',
+  },
+};
