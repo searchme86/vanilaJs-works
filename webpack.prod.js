b00faeca71e051bc416a2ef33cb5353c3bda696a
searchme86 <searchme86@gmail.com>
2022-11-20 02:24:07 +0900

    🔧 add webpack production config json

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,10 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
+  devtool: 'inline-source-map',
   entry: './src/index.ts',
   module: {
     rules: [
@@ -14,20 +17,44 @@ module.exports = {
         type: 'asset',
       },
       {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env'],
+            plugins: ['@babel/plugin-proposal-class-properties'],
+          },
+        },
+      },
+      {
         test: /\.ts?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
-        include: [path.resolve(__dirname, 'src')],
+        // include: [path.resolve(__dirname, 'src')],
       },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: ['.ts', '.js', 'scss'],
   },
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/dist',
   },
-  devtool: 'inline-source-map',
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css',
+    }),
+    new CleanWebpackPlugin(),
+  ],
 };
