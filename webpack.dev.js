d4c1901b1c2ceba0ceb88ba41e4fc047911266ea
searchme86 <searchme86@gmail.com>
2022-11-20 01:27:11 +0900

    🔧 add feature of clean-webpack-plugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -51,4 +52,5 @@ module.exports = {
     publicPath: '/dist',
   },
   plugins: [new TerserPlugin()],
+  plugins: [new TerserPlugin(), new CleanWebpackPlugin()],
 };
